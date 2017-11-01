4b04b47c691fd1046cc704560aae409dedc077ae
Aziz <aziz.yokubjonov@gmail.com>
2017-11-01 18:17:46 -0400

    retrieve projects from firebase

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
-//Unit 1 Portfolio with Firebase
-// Use Firebase to store the images links and info for your projects. 
-// Use Jquery to dynamically display the information from firebse. 
+let ref = firebase.database().ref('/');
 
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
+ref.on('value', (snapshot) => {
+    let profile = snapshot.val();
+    document.querySelector('.name').innerText = profile.name;
+    document.querySelector('.email').innerText = profile.email;
+    document.querySelector('.bio').innerText = profile.bio;
+});
 
+let projects = firebase.database().ref('/projects').on('value', (snapshot) => {
+    console.log(document.querySelector('.projects')[0]);
+    snapshot.forEach((child) => {
+        let project = child.val();
+        let projectContainer = document.createElement('a');
+        projectContainer.className = 'project';
+        projectContainer.href = project.url;
+        let img = document.createElement('img');
+        img.src = project.img;
+        let title = document.createElement('div');
+        title.innerText = project.name;
+        projectContainer.appendChild(img);
+        projectContainer.appendChild(title);
+        document.querySelector('.projects').appendChild(projectContainer);
+    });
+});
